cc86cc3337a6fba195a1094828dac6aca8f74450
erguerra <eduardo.ramalho@mercadolivre.com>
2022-02-16 23:16:52 -0300

    adds notification and creates foreground service
    creates a notification in order to maintaint the well behavior of the communication on higher api levels

--- app/src/main/java/com/github/erguerra/serviceone/ReceiverService.kt
+++ app/src/main/java/com/github/erguerra/serviceone/ReceiverService.kt
@@ -1,15 +1,29 @@
 package com.github.erguerra.serviceone
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 
 class ReceiverService : Service() {
 
     override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startForeground(3, createNotification())
+        }
+    }
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -34,6 +48,22 @@ class ReceiverService : Service() {
     private fun showResultAndCloseService(receivedModel: ReceivedData) {
         Toast.makeText(applicationContext, "Name: ${receivedModel.name} Age: ${receivedModel.age}", Toast.LENGTH_LONG).show()
         stopSelf()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotification() : Notification {
+        var builder = NotificationCompat.Builder(this, createNotificationChannel("ServiceOne", "ServiceOneChannel"))
+        return builder.build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val channel = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
     }
 
     companion object {
